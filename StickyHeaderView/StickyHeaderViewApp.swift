368015780182bd6dcdebacf2b80af0affcba5d45
jacostaf10 <jorge.acosta.freire@gmail.com>
2023-04-18 11:46:05 +0200

    Initial Commit

---
+++ StickyHeaderView/StickyHeaderViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickyHeaderViewApp.swift
+//  StickyHeaderView
+//
+//  Created by Jorge Acosta Freire on 18/4/23.
+//
+
+import SwiftUI
+
+@main
+struct StickyHeaderViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
